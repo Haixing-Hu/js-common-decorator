ae25279f6c1dcfefdd159533e3e47b2bdc96c6b5
Haixing Hu <starfish.hu@gmail.com>
2023-09-09 00:49:19 +0800

    refactor: @Log use console instead of logger

--- test/log.test.js
+++ test/log.test.js
@@ -6,32 +6,28 @@
  *    All rights reserved.
  *
  ******************************************************************************/
-import { logger, Logger } from '@haixing_hu/common-util/src/logger';
 import { mount } from '@vue/test-utils';
 import Foo from './model/foo';
 import Hello from './model/hello-vue';
 
-const myLogger = new Logger('DEBUG');
 let logs = [];        // 记录所有的日志
-const appender = {
+window.console = {          // 重载 window.console
   debug: (...args) => {
     logs.push({ type: 'DEBUG', args });
-    myLogger.debug(args.slice(2).join(' '));
   },
   info: (...args) => {
     logs.push({ type: 'INFO', args });
-    myLogger.info(args.slice(2).join(' '));
   },
   warn: (...args) => {
     logs.push({ type: 'WARN', args });
-    myLogger.warn(args.slice(2).join(' '));
   },
   error: (...args) => {
     logs.push({ type: 'ERROR', args });
-    myLogger.error(args.slice(2).join(' '));
+  },
+  log: (...args) => {
+    logs.push({ type: 'LOG', args });
   },
 };
-logger.setAppender(appender);   // 设置自定义的 appender
 
 /**
  * 单元测试 @Log 装饰器。
@@ -43,14 +39,18 @@ describe('Test @Log decorator for class method', () => {
     logs = [];
     const foo = new Foo();
     foo.say('Hello');
-    expect(logs.length).toBe(1);
+    expect(logs.length).toBe(2);
     expect(logs[0]).toEqual({
       type: 'DEBUG',
       args: [
-        '[DEBUG]',
-        logger.lastTimestamp,
         'Foo.say:',
         'Hello',
+      ],
+    });
+    expect(logs[1]).toEqual({
+      type: 'LOG',
+      args: [
+        'Foo.say: Hello',
       ],
     });
   });
@@ -59,21 +59,26 @@ describe('Test @Log decorator for class method', () => {
     logs = [];
     const foo = new Foo();
     foo.add(1, 2);
-    expect(logs.length).toBe(2);
-    expect(logs[0].type).toBe('DEBUG');
-    expect(logs[0].args.length).toBe(6);
-    expect(logs[0].args[0]).toBe('[DEBUG]');
-    expect(logs[0].args[2]).toBe('Foo.add:');
-    expect(logs[0].args[3]).toEqual(1);
-    expect(logs[0].args[4]).toEqual(',');
-    expect(logs[0].args[5]).toEqual(2);
+    expect(logs.length).toBe(3);
+    expect(logs[0]).toEqual({
+      type: 'DEBUG',
+      args: [
+        'Foo.add:',
+        1,
+        2,
+      ],
+    });
     expect(logs[1]).toEqual({
       type: 'DEBUG',
       args: [
-        '[DEBUG]',
-        logger.lastTimestamp,
         'Foo.say:',
         'Foo.add: 1, 2',
+      ],
+    });
+    expect(logs[2]).toEqual({
+      type: 'LOG',
+      args: [
+        'Foo.say: Foo.add: 1, 2',
       ],
     });
   });
@@ -93,41 +98,63 @@ describe('Test @Log decorator for Vue method', () => {
     const p = hello.findComponent('p');
     expect(p.text()).toBe('Hello World!');
     // 检查 Hello.created() 的日志
-    expect(logs.length).toBe(1);
+    expect(logs.length).toBe(2);
     expect(logs[0]).toEqual({
       type: 'DEBUG',
       args: [
-        '[DEBUG]',
-        logger.lastTimestamp,
         'Hello.created.',
+      ],
+    });
+    expect(logs[1]).toEqual({
+      type: 'LOG',
+      args: [
+        'In Function: Hello.created.',
       ],
     });
     // 检查 Hello.foo() 的日志
     hello.vm.foo(1, 2);
-    expect(logs.length).toBe(2);
-    expect(logs[1]).toEqual({
+    expect(logs.length).toBe(5);
+    expect(logs[2]).toEqual({
       type: 'DEBUG',
       args: [
-        '[DEBUG]',
-        logger.lastTimestamp,
         'Hello.foo:',
         1,
-        ',',
+        2,
+      ],
+    });
+    expect(logs[3]).toEqual({
+      type: 'LOG',
+      args: [
+        'In Function: Hello.Foo: 1, 2',
+      ],
+    });
+    expect(logs[4]).toEqual({
+      type: 'LOG',
+      args: [
+        'In Function: ',
+        'Hello World!',
+        ' x = ',
+        1,
+        ' y = ',
         2,
       ],
     });
     // 检查 Hello.add(1, 2)的结果
     const result = hello.vm.add(1, 2);
     expect(result).toBe(3);
-    expect(logs[2]).toEqual({
+    expect(logs.length).toBe(7);
+    expect(logs[5]).toEqual({
       type: 'DEBUG',
       args: [
-        '[DEBUG]',
-        logger.lastTimestamp,
         'Hello.add:',
         1,
-        ',',
         2,
+      ],
+    });
+    expect(logs[6]).toEqual({
+      type: 'LOG',
+      args: [
+        'In Function: Hello.add: 1, 2',
       ],
     });
   });
