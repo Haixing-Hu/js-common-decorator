402664f58c5e9b8c424fd320bd7a3adce9ca68c8
Haixing Hu <starfish.hu@gmail.com>
2023-10-25 17:36:51 +0800

    build: refactor the building system

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//    Copyright (c) 2022 - 2023.
+//    Haixing Hu, Qubit Co. Ltd.
+//
+//    All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+module.exports = {
+  verbose: true,
+  testMatch: ['**/test/**/*.test.js'],
+  testEnvironment: 'jest-environment-jsdom-global',
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'node-addons'],
+  },
+  moduleFileExtensions: ['js', 'vue'],
+  transform: {
+    '\\.js$': 'babel-jest',
+    '^.+\\.vue$': '@vue/vue3-jest',
+  },
+  setupFilesAfterEnv: [
+    'jest-extended/all',
+  ],
+  collectCoverage: true,
+  coverageDirectory: './coverage',
+  collectCoverageFrom: ['**/src/**/*.js'],
+};
