649b3aa0979aff89d82cc59bc2ea688b2d30aabe
Haixing Hu <starfish.hu@gmail.com>
2024-09-13 11:31:11 +0800

    test: add a unit test

--- test/model.clone.test.js
+++ test/model.clone.test.js
@@ -6,6 +6,7 @@
 //    All rights reserved.
 //
 ////////////////////////////////////////////////////////////////////////////////
+import { DefaultOptions } from '../src';
 import Credential from './model/credential';
 import CredentialType from './model/credential-type';
 import Person from './model/person';
@@ -34,5 +35,32 @@ describe('Test the prototype method `clone()`', () => {
     expect(result.credential).toBeInstanceOf(Credential);
     expect(result.credential.type).toBe(CredentialType.PASSPORT);
     expect(result.credential.number).toBe(data.credential.number);
+  });
+
+  test('Test `Person.prototype.clone()` never convert naming', () => {
+    const data = {
+      id: 'xxxxx',
+      name: 'Bill Gates',
+      age: 55,
+      mobile: '139280384745',
+      credential: {
+        type: 'PASSPORT',
+        number: 'xx1234567',
+      },
+    };
+    const person = new Person();
+    person.assign(data, { normalize: false });
+    DefaultOptions.set('assign', { convertNaming: true });
+    const result = person.clone();
+    expect(result).toBeInstanceOf(Person);
+    expect(result.id).toBe(data.id);
+    expect(result.name).toBe(data.name);
+    expect(result.age).toBe(data.age);
+    expect(result.gender).toBe('');
+    expect(result.mobile).toBe(data.mobile);
+    expect(result.credential).toBeInstanceOf(Credential);
+    expect(result.credential.type).toBe(CredentialType.PASSPORT);
+    expect(result.credential.number).toBe(data.credential.number);
+    DefaultOptions.reset();
   });
 });
